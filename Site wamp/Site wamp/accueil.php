6b9325888eaee5b2d0ca469c58e5e01b7725e92f
Remi-91 <62675393+Remi-91@users.noreply.github.com>
2020-04-10 17:01:45 +0200

    Add files via upload
    suite de suppression de la variable 'connecte'

---
+++ Site wamp/Site wamp/accueil.php
@@ -0,0 +1,53 @@
+<?php
+	session_start();
+?>
+
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/accueil.css">
+	<title>Accueil</title>
+</head>
+
+<body>
+	<header>
+		<div class="header">
+			<a class="NomDuSite" href="#">Card<span>Games</span></a>
+			<img src="img/question.jpg" width=100% height="400">
+			<div class="nav">
+				<ul>
+					<li><a href="#">Accueil</a></li>
+					<li><a href="jouer.php">Jouer</a></li>
+					<li><a href="informations.php">Informations</a></li>
+					<li><a href="connexion.php">Connexion</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	
+
+	</header>
+
+
+	<div class="contenu">
+		<p>test de contenu</p>
+		<p>Un second test</p>
+	</div>
+
+
+
+
+	<footer>
+
+		<?php
+		if(isset($_SESSION['pseudo']) && isset($_SESSION['email']) && isset($_SESSION['sexe'])){
+			echo "L'utilisateur ",$_SESSION['pseudo']," ",$_SESSION['email']," de sexe ",$_SESSION['sexe']," vient de créer un nouveau compte.";
+		}
+		?>
+
+		<p>CardGame</p>
+	</footer>
+</body>
+
+</html>
---
+++ Site wamp/Site wamp/jouer.php
@@ -0,0 +1,34 @@
+<?php
+	session_start();
+	
+?>
+
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/jouer.css">
+	<title>Jouer</title>
+</head>
+
+<body>
+	<header>
+		<div class="header">
+			<a class="NomDuSite "href="accueil.php">Card<span>Games</span></a>
+			<img src="img/question.jpg" width=100% height="400">
+			<div class="nav">
+				<ul>
+					<li><a href="accueil.php">Accueil</a></li>
+					<li><a href="#">Jouer</a></li>
+					<li><a href="informations.php">Informations</a></li>
+					<li><a href="connexion.php">Connexion</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	
+
+	</header>
+</body>
+
+</html>
