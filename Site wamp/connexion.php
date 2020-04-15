9aac6ef2a02c2bb2f2761b9042fba315cc939853
Remi-91 <62675393+Remi-91@users.noreply.github.com>
2020-04-15 19:02:57 +0200

    Update connexion.php

--- Site wamp/connexion.php
+++ Site wamp/connexion.php
@@ -14,7 +14,7 @@
 	<body>
 		<header>
 			<div class="header">
-				<a class="NomDuSite "href="accueil.php">Card<span>Games</span></a>
+				<a class="NomDuSite "href="accueil.php">Card<span>Game</span></a>
 				<img src="img/question.jpg" width=100% height="400">
 				<div class="nav">
 					<ul>
@@ -22,7 +22,7 @@
 						<li><a href="jouer.php">Jouer</a></li>
 						<li><a href="informations.php">Informations</a></li>
 						<li><a href="#">Connexion</a></li>
-						<li><a href="contact.php">Contact</a></li>
+						<li><a href="avis.php">Vos avis</a></li>
 					</ul>
 				</div>
 			</div>
